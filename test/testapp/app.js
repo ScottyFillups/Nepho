deb020ba932e74f20904588b8b70a81400d84a4b
Philip Scott <pscott@zeptohost.com>
2017-06-29 22:36:28 -0400

    Iterate through api object properties

--- test/testapp/app.js
+++ test/testapp/app.js
@@ -46,21 +46,4 @@ app.use(function(err, req, res, next) {
 
 app.listen(8080);
 
-/*app.get("/", function(req, res) {
-    fonoapi.token = "9aaca61f8fad680548561fd336228f508d5e24a6bde084cb";
-    res.writeHead(200, {"Content-Type": "text/html"});
-
-    fonoapi.getDevices(function(queryString, data) {
-        for (prop in data[0]) {
-            res.write("<p>" + prop + " : " + data[0][prop] + "</p>");
-        }
-    }, "iphone 6S");
-}).listen(8080);
-*/
-
-
-
-
-
-
 module.exports = app;
