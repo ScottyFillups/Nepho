0a3ced7a093fb202194e48a1135c207602ec5836
Philip Scott <pscott@zeptohost.com>
2017-06-29 22:01:07 -0400

    Add testing for express generator

---
+++ test/testapp/app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var path = require('path');
+//var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+app.listen(8080);
+
+/*app.get("/", function(req, res) {
+    fonoapi.token = "9aaca61f8fad680548561fd336228f508d5e24a6bde084cb";
+    res.writeHead(200, {"Content-Type": "text/html"});
+
+    fonoapi.getDevices(function(queryString, data) {
+        for (prop in data[0]) {
+            res.write("<p>" + prop + " : " + data[0][prop] + "</p>");
+        }
+    }, "iphone 6S");
+}).listen(8080);
+*/
+
+
+
+
+
+
+module.exports = app;
---
+++ test/testapp/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var fonoapi = require('fonoapi-nodejs');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get("/", function(req, res) {
+    fonoapi.token = "9aaca61f8fad680548561fd336228f508d5e24a6bde084cb";
+    fonoapi.getDevices(function(queryString, data) {
+        res.render('index', data[0]);
+    }, "iphone 6S");
+})
+
+module.exports = router;
